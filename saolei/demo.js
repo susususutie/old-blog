ca341cd1a0c0057988928acc326d2e3440599c2a
苏苏苏苏铁 <sususu_sutie@qq.com>
2019-03-27 16:42:15 +0800

    0327
    原生JS做的扫雷小游戏，花了两天终于做完了

---
+++ saolei/demo.js
@@ -0,0 +1,240 @@
+//点击start显示game区域（去除hidden），随机给item添加lei，开始计时
+
+//左键 -- 有旗   --- 无反应
+//     -- 有lei  --- 败
+//     -- 没有lei -- 显示周围雷数 -- 数字
+//                                                            -- 0 递归查看直到不为0
+//右键 -- 有旗 ---- 取消旗
+//     -- 没有旗 -- 插旗 ---所有旗都对 -- 胜
+//     -- 数字或0 - 无反应
+
+var timeNum = document.getElementsByClassName("timeNum")[0];
+var start = document.getElementsByClassName("start")[0];
+var game = document.getElementsByClassName("game")[0];
+var alertCon = document.getElementsByClassName("alertCon")[0];
+var close = document.getElementsByClassName("close")[0];
+
+var item = document.getElementsByClassName("item");
+
+var flag;
+var minMap = new Array(81);
+var numMap = new Array(81);
+
+function csh () {
+    for (var i = 0; i < 81; i++) {
+        minMap[i] = 0;
+        numMap[i] = 0;
+    }
+    flag = 20;
+    flagNum.innerHTML = flag;
+    var quqi = document.getElementsByClassName("qied");
+    var qul = quqi.length;
+    for (var i = 0; i < qul; i++) {
+        quqi[i].classList.remove("qied");
+    }
+    var cheIte = document.getElementsByClassName("checked");
+    var chl = cheIte.length;
+    for (var i = 0; i < chl; i++) {
+        cheIte[i].classList.remove("checked");
+    }
+    
+}
+
+var key = true;
+
+allEvent ();
+function allEvent () {
+    // csh();
+    start.addEventListener("click", function () {
+        csh();
+        if (key) {
+            console.log(timeNum + "开始计时，" + "初始化")
+            game.classList.remove("hidden")
+            
+            lei();
+            key = false;
+        }
+        
+    })
+
+    //取消右键
+    game.oncontextmenu = function () {
+        return false;
+    }
+
+    //事件委托找到被点击的小方格,调用对应左键和右键函数
+    game.addEventListener("mousedown", function (e) {
+        targetItem = e.target;
+        var itemNumber = Array.prototype.indexOf.call(item, targetItem);
+        if (e.which == 1 && !item[itemNumber].classList.contains("qied")) {
+            leftClick(itemNumber);
+        }else if (e.which == 3 && flag > 0) {
+            rightClick(itemNumber);
+        }
+    })
+
+    close.addEventListener("click", function () {
+        key = true;
+        for (var i = 0; i < 81; i++) {
+            item[i].innerHTML = "";
+            if (minMap[i] == 1) {
+                item[i].classList.remove("isLei");
+            }
+        }
+        alertCon.classList.add("hidden");
+        game.classList.add("hidden");
+        
+    })
+}
+
+
+//随机确定雷的位置
+function lei() {
+    for (i = 0; i < flag; i++) {
+        var n = Math.floor(Math.random() * 81);
+        if (minMap[n] == 0) {
+            item[n].classList.add("isLei");
+            minMap[n] = 1;
+        }else {
+            i--;
+        }
+    }
+    for (n = 0; n < 81; n++) {
+        if (minMap[n] == 0) {
+            switch (n) {
+                case 9:
+                case 18:
+                case 27:
+                case 36:
+                case 45:
+                case 54:
+                case 63:
+                numMap[n] = num(minMap[n-9]) + num(minMap[n-8]) + num(minMap[n+1]) + num(minMap[n+9]) + num(minMap[n+10]);
+                break;
+                case 17:
+                case 26:
+                case 35:
+                case 44:
+                case 53:
+                case 62:
+                case 71:
+                numMap[n] = num(minMap[n-10]) + num(minMap[n-9]) + num(minMap[n-1]) + num(minMap[n+8]) + num(minMap[n+9]);
+                break;
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                case 5:
+                case 6:
+                case 7:
+                numMap[n] =num(minMap[n-1]) + num(minMap[n+1]) + num(minMap[n+8]) + num(minMap[n+9]) + num(minMap[n+10]);
+                break;
+                case 73:
+                case 74:
+                case 75:
+                case 76:
+                case 77:
+                case 78:
+                case 79:
+                numMap[n] = num(minMap[n-10]) + num(minMap[n-9]) + num(minMap[n-8]) + num(minMap[n-1]) + num(minMap[n+1]);
+                break;
+                case 0:
+                numMap[n] = num(minMap[1]) + num(minMap[9]) + num(minMap[10]);
+                break;
+                case 8:
+                numMap[n] = num(minMap[7]) + num(minMap[16]) + num(minMap[17]);
+                break;
+                case 72:
+                numMap[n] = num(minMap[63]) + num(minMap[64]) + num(minMap[73]);
+                break;
+                case 80:
+                numMap[n] = num(minMap[70]) + num(minMap[71]) + num(minMap[79]);
+                break;
+                default:
+                numMap[n] = num(minMap[n-10]) + num(minMap[n-9]) + num(minMap[n-8]) + num(minMap[n-1]) + num(minMap[n+1]) + num(minMap[n+8]) + num(minMap[n+9]) + num(minMap[n+10]);
+            }
+        }
+    }
+}
+
+//num()
+function num(a) {
+    var value = Number(a);
+    if (Number.isNaN(value)) {
+        value = 0;
+    }
+    return value;
+}
+
+//
+function leftClick(itemNumber) {
+
+    item[itemNumber].classList.add("checked");
+    var pos = item[itemNumber].getAttribute("id").split("-");
+    var x = pos[0];
+    var y = pos[1];
+    //没雷
+    if (minMap[itemNumber] == 0) {
+        item[itemNumber].innerHTML = numMap[itemNumber];
+        //判断周边
+        if (numMap[itemNumber] == 0) {
+            for (var i = x - 1; i <= x + 1; i++) {
+                for (var j = y - 1; j <= y + 1; j++) {
+                    var near = document.getElementById(i + "-" + j);
+                    if (near) {
+                        var nearNum = Array.prototype.indexOf.call(item, near);
+                        if (minMap[nearNum] == 0 && !near.classList.contains("checked")) {
+                            leftClick(nearNum);
+                        }
+                    }
+                }
+            }
+            
+            
+        }
+    } else {
+        for (var i = 0; i < 81; i++) {
+            if (item[i].classList.contains("qied")) {
+                item[i].classList.remove("qied");
+            }
+            if (minMap[i] == 0) {
+                item[i].innerHTML = numMap[i];
+            } else {
+                item[i].innerHTML = "💥";
+            }
+        }
+        setTimeout(function () {
+            alertCon.classList.remove("hidden");
+        }, 555)
+    }
+}
+
+//右键函数
+function rightClick (itemNumber) {
+    if (!item[itemNumber].classList.contains("qied")) {
+        if (!item[itemNumber].classList.contains("checked")) {
+            item[itemNumber].classList.add("qied");
+            flag--;
+            flagNum.innerHTML = flag;
+        }
+        
+        if (flag == 0) {
+            var qizi = document.getElementsByClassName("qied");
+            var n = 0;
+            for (var i = 0; i < 20; i++) {
+                if (qizi[i].classList.contains("isLei")) {
+                    n++;
+                }
+            }
+            if (n == 20) {
+                console.log("sheng");
+            }
+        }
+        
+    }else {
+        item[itemNumber].classList.remove("qied")
+        flag++;
+        flagNum.innerHTML = flag;
+    }
+    
+}
---
+++ saolei/demo1.js
@@ -0,0 +1,211 @@
+var timeNum = document.getElementsByClassName("timeNum")[0];
+var flagNum = document.getElementsByClassName("flagNum")[0];
+var start = document.getElementsByClassName("start")[0];
+var game = document.getElementsByClassName("game")[0];
+var item = document.getElementsByClassName("item");
+var alertCon = document.getElementsByClassName("alertCon")[0];
+var msg = document.getElementsByClassName("msg")[0];
+var close = document.getElementsByClassName("close")[0];
+
+bindEvent();
+
+//计时器
+var c = 0;
+var t;
+
+var flag = 20;
+
+//清除items的class、计时器
+function clearItemcls () {
+    c = 0;
+    clearInterval(t);
+    flag = 20;
+    flagNum.innerHTML = 20;
+    for (var i = 0; i < item.length; i++) {
+        if (item[i].classList.contains("isLei")) {
+            item[i].classList.remove("isLei");
+        }
+        if (item[i].classList.contains("dianEd")) {
+            item[i].classList.remove("dianEd");
+        }
+        if (item[i].classList.contains("qiEd")) {
+            item[i].classList.remove("qiEd");
+        }
+        if (item[i].classList.contains("showLei")) {
+            item[i].classList.remove("showLei");
+        }
+        item[i].innerHTML = "";
+    }
+    
+}
+
+function jiShi() {
+    timeNum.innerHTML = c
+    c = c + 1
+    t = setTimeout("jiShi()", 1000)
+}
+// clearInterval(t);
+
+//xuanLei选雷[0, 80],不是雷让它变雷，是雷重选
+function xuanLei (leishu) {
+    for (var i = 0; i < leishu; i++) {
+        var suijishu = Math.floor(Math.random() * 81);
+        if (!item[suijishu].classList.contains("isLei")) {
+            item[suijishu].classList.add("isLei");
+        }else {
+            i--;
+        }
+    }
+}
+
+//showNum没被点开，没插旗，不是雷，显示数字
+function showNum (targetItem) {
+    var pos = targetItem.getAttribute("id").split("-");
+    var posx = pos[0];
+    var posy = pos[1];
+    var n = 0;
+
+    for (var i = posx - 1; (i - 1) <= posx; i++) {
+        for (var j = posy - 1; (j - 1) <= posy; j++) {
+            var near = document.getElementById(i + "-" + j);
+            if (near && near.classList.contains("isLei")) {
+                n++;
+            }
+        }
+    }
+    targetItem && (targetItem.innerHTML = n);
+    if (n == 0) {
+        for (var i = posx - 1; (i - 1) <= posx; i++) {
+            for (var j = posy - 1; (j - 1) <= posy; j++) {
+                var near = document.getElementById(i + "-" + j);
+                if (near && !near.classList.contains("isLei") && !near.classList.contains("qiEd")) {
+                    leftClick(near);
+                }
+            }
+        }
+    }
+}
+
+//lose
+function loseGame () {
+    clearInterval(t);
+    msg.innerHTML = "扫雷失败"
+    setTimeout(function () {
+        alertCon.classList.remove("hidden");
+    }, 300)
+    showLei();
+}
+
+//win
+function winGame () {
+    var endTime = Number.parseFloat(timeNum.innerHTML);
+    clearInterval(t);
+    msg.innerHTML = `扫雷成功，你用时${endTime}S`
+    setTimeout(function () {
+        alertCon.classList.remove("hidden");
+    }, 300)
+    showLei();
+}
+
+//显示所有雷
+function showLei () {
+    var alllei = document.getElementsByClassName("isLei");
+    for (var i = 0; i < alllei.length; i++) {
+        if (alllei[i].classList.contains("qiEd")) {
+            alllei[i].classList.remove("qiEd");
+        }
+        alllei[i].classList.add("showLei");
+    }
+}
+
+//左键函数
+function leftClick (targetItem) {
+    if (!targetItem.classList.contains("dianEd") && !targetItem.classList.contains("qiEd")) {
+        if (!targetItem.classList.contains("isLei")) {
+            targetItem.classList.add("dianEd");
+            showNum(targetItem);
+            winOrNot(1);
+            //判断，所有的数都显示，剩下全都是雷，win
+        }else {
+            loseGame();
+        }
+    }
+}
+
+//取消右键
+game.oncontextmenu = function () {
+    return false;
+}
+
+//右键函数
+function rightClick (targetItem) {
+    if (!targetItem.classList.contains("dianEd")) {
+        if (!targetItem.classList.contains("qiEd") && flag > 0) {
+            targetItem.classList.add("qiEd");
+            flag--;
+        }else if (targetItem.classList.contains("qiEd")) {
+            targetItem.classList.remove("qiEd");
+            flag++;
+        }
+    }
+    flagNum.innerHTML = flag;
+}
+
+//1、旗子插完后，判断是否全对，2、点开一个格子，判断是否所有非雷的格子都被点开
+function winOrNot (leftorright) {
+    var islei = document.getElementsByClassName("isLei");
+    if (leftorright == 3) {
+        
+        var qied = document.getElementsByClassName("qiEd");
+        var n = 0;
+        for (var i = 0; i < qied.length; i++) {
+            if (qied[i].classList.contains("isLei")) {
+                n++;
+            }
+        }
+        if (n == 20) {
+            winGame();
+        }
+    }else if (leftorright == 1) {
+        console.log("zuojian")
+        var dianed = document.getElementsByClassName("dianEd");
+        console.log("dianEd " + dianed.length);
+        console.log("islei " + islei.length);
+        if (dianed.length + islei.length == 81) {
+            console.log("win");
+            winGame();
+        }
+    }
+}
+
+//事件绑定
+function bindEvent() {
+    // start-btn
+    start.addEventListener("click", function () {
+        clearItemcls ();
+        game.classList.remove("hidden");
+        jiShi();
+        xuanLei(20);
+    })
+
+    game.addEventListener("mousedown", function (e) {
+        targetItem = e.target;
+        if (e.which == 1) {
+            leftClick(targetItem);
+        }else if (e.which == 3) {
+            rightClick(targetItem);
+            if (flag == 0) {
+                winOrNot(3);
+            }
+        }
+    })
+
+    // close-btn
+    close.addEventListener("click", function () {
+        alertCon.classList.add("hidden");
+        game.classList.add("hidden");
+        timeNum.innerHTML = 0;
+    })
+
+
+}
